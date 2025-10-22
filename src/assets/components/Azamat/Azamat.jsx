5ab9dfed6e95f2ce105ddc562a2942f306595ed8
Abdullohabdumalikov <a3766668@gmail.com>
2025-10-22 10:10:06 +0500

    done structure

---
+++ src/assets/components/Azamat/Azamat.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../Azamat/azamat.css'
+function Azamat() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Azamat
---
+++ src/assets/components/Man/Man.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../Man/man.css'
+function Man() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Man
