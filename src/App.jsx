cb6be77b48b4ab2b2314d8d460fac897d15c344a
Abdullohabdumalikov <a3766668@gmail.com>
2025-10-24 09:53:14 +0500

    asdvasdvasdv

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import "./App.css";
-<<<<<<< HEAD
 import Header from "./assets/components/Header/Header";
 import Main from "./assets/components/Main/Main";
 import Man from "./assets/components/Man/Man";
@@ -18,18 +17,6 @@ function App() {
           </div>
         </div>
       </main>
-=======
-import Azamat from "./assets/components/Azamat/Azamat";
-
-function App() {    
-  return (
-    <>
-      <div className="container">
-        <div className="aza">
-          <Azamat />
-        </div>{" "}
-      </div>{" "}
->>>>>>> f3ee6232500012e60314d55586efb8ebd6188509
     </>
   );
 }
